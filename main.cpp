44172c5ad310c84ab5dd0ab4c0d9bddcec97b45b
Hlanden5 <91215833+Hlanden5@users.noreply.github.com>
2023-10-24 02:16:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,4 @@
-#include <iostream>
-#include <sys/stat.h>
-#include <fstream>
-#include <dlfcn.h>
-#include <algorithm>
-#include <cctype>
-#include <unistd.h>
-#include <set>
-
 #include <libaltrepo.h>
-
-
-void downloadPacketsInfo(std::vector<std::string>& branch_vec);
-void parseFile(std::string branch, std::vector<packetInfo> &packets);
-void getAllArch(std::vector<std::string>& arch_vec, const std::vector<packetInfo>& branch);
 
 int main()
 {
@@ -33,6 +19,8 @@ int main()
         if(packet2!=1 and packet2!=2 and packet2!=3)
             std::cout << "Check second binary branch!";
     }while(packet1==packet2);
+
+    std::cout << "Downloading binary branches" << std::endl;
 
     switch(packet1){
     case 1:
@@ -73,27 +61,44 @@ int main()
     }
 
     char path[1024];
-    std::ofstream makeFile{std::string(getcwd(path, sizeof(path))) + std::string("/check.json"), std::ios_base::app};
-    makeFile.close();
-
-    std::ofstream json{std::string(getcwd(path, sizeof(path))) + std::string("/check.json"), std::ios_base::trunc};
 
     std::vector<std::string> arch_vec;
     getAllArch(arch_vec, branch1);
 
-    std::multiset<packetInfo, packetInfoComp> branch1_set(branch1.begin(), branch1.end());
-    std::multiset<packetInfo, packetInfoComp> branch2_set(branch2.begin(), branch2.end());
+    std::multiset<packetInfo, packetInfoComp> branch1_multiset(branch1.begin(), branch1.end());
+    std::multiset<packetInfo, packetInfoComp> branch2_multiset(branch2.begin(), branch2.end());
+    branch1.clear();
+    branch2.clear();
 
     for(auto j = arch_vec.begin(); j < arch_vec.end(); ++j){
 
+        std::string tmp{"/"+branch_vec[0]+"-"+branch_vec[1]+"-"+*j+".json"};
+
+        std::ofstream makeFile{std::string(getcwd(path, sizeof(path))) + tmp, std::ios_base::app};
+        makeFile.close();
+
+        std::ofstream json{std::string(getcwd(path, sizeof(path))) + tmp, std::ios_base::trunc};
+
+        std::multiset<packetInfo, packetInfoComp> branch1_arch_set, branch2_arch_set;
+
         // 1
-        json << "{\"arch\": " << *j << "} ";
+        json << "{\"arch\": " << *j << ", ";
         json << "{\"All packages that are in the 1st but not in the 2nd\", "
              << "\"packages\": [";
 
-        std::vector<packetInfo> diff;
-        std::set_difference(branch1_set.begin(), branch1_set.end(), branch2_set.begin(), branch2_set.end(), std::back_inserter(diff), packetInfoComp());
+        for (const auto& i : branch1_multiset) {
+                if (i.arch == *j) {
+                    branch1_arch_set.insert(i);
+                }
+            }
+            for (const auto& i : branch2_multiset) {
+                if (i.arch == *j) {
+                    branch2_arch_set.insert(i);
+                }
+            }
 
+        std::vector<packetInfo> diff;
+        std::set_difference(branch1_arch_set.begin(), branch1_arch_set.end(), branch2_arch_set.begin(), branch2_arch_set.end(), std::back_inserter(diff), packetInfoComp());
         for (const auto& i : diff) {
             json << "{" << "\"name\": " << i.name << ", "
                  << "\"epoch\": " << i.epoch << ", "
@@ -108,12 +113,12 @@ int main()
         json << "]}" << std::endl;
 
         // 2
-        json << "{\"arch\": " << *j << "} ";
-        json << "{\"All packages that are in the 2st but not in the 1nd\", "
+        json << "{\"arch\": " << *j << ", ";
+        json << "\"All packages that are in the 2st but not in the 1nd\", "
              << "\"packages\": [";
 
         diff.clear();
-        std::set_difference(branch2_set.begin(), branch2_set.end(), branch1_set.begin(), branch1_set.end(), std::back_inserter(diff), packetInfoComp());
+        std::set_difference(branch2_arch_set.begin(), branch2_arch_set.end(), branch1_arch_set.begin(), branch1_arch_set.end(), std::back_inserter(diff), packetInfoComp());
 
         for (const auto& i : diff) {
             json << "{" << "\"name\": " << i.name << ", "
@@ -129,13 +134,13 @@ int main()
 
         // 3
 
-        json << "{\"arch\": " << *j << "} ";
-        json << "{\"All packages whose version-release is greater in the 1st than in the 2nd\", "
+        json << "{\"arch\": " << *j << ", ";
+        json << "\"All packages whose version-release is greater in the 1st than in the 2nd\", "
              << "\"packages\": [";
 
         diff.clear();
-        std::set_difference(branch1_set.begin(), branch1_set.end(), branch2_set.begin(), branch2_set.end(), std::back_inserter(diff), packetInfoCompVerRel());
 
+        std::set_difference(branch1_arch_set.begin(), branch1_arch_set.end(), branch2_arch_set.begin(), branch2_arch_set.end(), std::back_inserter(diff), packetInfoCompVerRel());
         for (const auto& i : diff) {
             json << "{" << "\"name\": " << i.name << ", "
                  << "\"epoch\": " << i.epoch << ", "
@@ -147,76 +152,10 @@ int main()
                  << "\"source\": " << i.source << "}, ";
         }
         json << "]}" << std::endl;
+        json.close();
     }
-    json.close();
+
 
     std::cout << "Job done!" << std::endl;
     return 0;
-}
-
-void getAllArch(std::vector<std::string>& arch_vec, const std::vector<packetInfo> &branch){
-    for(auto i = branch.begin();i<branch.end();++i){
-        if(std::count(arch_vec.begin(),arch_vec.end(),i->arch) == 0){
-            arch_vec.push_back(i->arch);
-        }
-    }
-}
-
-void downloadPacketsInfo(std::vector<std::string>& branch_vec){
-    system("mkdir -p /tmp/ALTREPO");
-    LibALTrepo repo;
-    for(auto& c:branch_vec){
-        repo.setBranch(c);
-        repo.downloadFile();
-        std::ifstream file(repo.getPathJSON());
-        file.seekg (0, std::ios::end);
-        std::streampos size = file.tellg();
-        file.close();
-        if(size>200){
-            std::cout << "Branch "<< c << " downloaded, size = " << size/1024 << " Kb" << std::endl;
-        }else{
-            std::cout << "Branch don't downloaded, check log's in /tmp/ALTREPO" << std::endl;
-        }
-    }
-}
-
-void parseFile(std::string branch, std::vector<packetInfo> &packets){
-    std::string filename{"/tmp/ALTREPO/"};
-    filename += branch;
-    std::ifstream file(filename);
-
-    if(file.is_open()){
-        std::vector<std::string> strings;
-        std::string line;
-        while (std::getline(file, line, '}')) {
-                strings.push_back(line + '}');
-            }
-        strings.resize(strings.size()-2);
-        int i = 0;
-        for(auto&c :strings ){
-
-            auto findField = [&](const std::string& field) {
-                return c.find("\"" + field + "\"") + field.size() + 4;
-            };
-
-            auto extractValue = [&](size_t pos) {
-                size_t endPos = c.find_first_of(",}", pos);
-                return c.substr(pos, endPos - pos);
-            };
-
-            packetInfo info;
-            info.name = extractValue(findField("name"));
-            info.epoch = extractValue(findField("epoch"));
-            info.version = extractValue(findField("version"));
-            info.release = extractValue(findField("release"));
-            info.arch = extractValue(findField("arch"));
-            info.disttag = extractValue(findField("disttag"));
-            info.buildtime = extractValue(findField("buildtime"));
-            info.source = extractValue(findField("source"));
-            if(i!=0){
-                packets.push_back(info);
-            }else
-                i++;
-        }
-    }
 }
